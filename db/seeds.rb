85f54a35c1436cf51688229d90a3192bb00a6d92
irriz-jayy <jayfelix789@gmail.com>
2023-02-28 19:12:54 +0300

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,16 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+20.times do
+    Author.create(name: Faker::Name.name)
+end
+
+20.times do
+    Blog.create(
+        title: Faker::Book.title,
+        content: Faker::Lorem.paragraph,
+        author_id: rand(Author.first.id..Author.last.id)
+    )
+end
+
 puts "✅ Done seeding!"
